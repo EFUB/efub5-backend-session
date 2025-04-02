9541ffd51fba6603597254e0b08a421c9d4e5a06
s2eojeong <155219307+s2eojeong@users.noreply.github.com>
2025-04-02 23:44:28 +0900

    fix: @transactional 추가

--- efubaccount/src/main/java/com/practice/efubaccount/service/AccountsService.java
+++ efubaccount/src/main/java/com/practice/efubaccount/service/AccountsService.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AccountsService {
 
     private final AccountsRepository accountsRepository;
