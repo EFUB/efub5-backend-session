a21d9b13b03f2916b54181862a586b02c27864df
m2nsp <mini071663@gmail.com>
2025-04-02 15:57:07 +0900

    ✨feat: week4-seminar

--- efubaccount/src/main/java/com/practice/efubaccount/repository/AccountsRepository.java
+++ efubaccount/src/main/java/com/practice/efubaccount/repository/AccountsRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+//jpaRepository를 상속받으면 굳이 구현체를 만들어서 쓰지 않아도 사용가능함
 public interface AccountsRepository extends JpaRepository<Account, Long> {
 
-//    // 이메일 중복검사를 위한 쿼리
-//    boolean existsByEmail(String email);
-//
-//    // 회원 ID로 조회
-//    Optional<Account> findByAccountId(Long accountId);
+    // 이메일 중복검사를 위한 쿼리
+    boolean existsByEmail(String email);
+
+    // 회원 ID로 조회
+    Optional<Account> findByAccountId(Long accountId);
 
 }
