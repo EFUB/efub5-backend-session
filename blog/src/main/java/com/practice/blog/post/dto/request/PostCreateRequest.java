10e943f6393c86bb11149ecf2fa7f806fba1f5b6
sohyu-na <solim73@naver.com>
2025-04-10 17:21:44 +0900

    feat : post CRUD api

--- blog/src/main/java/com/practice/blog/post/dto/request/PostCreateRequest.java
+++ blog/src/main/java/com/practice/blog/post/dto/request/PostCreateRequest.java
@@ -1,14 +1,21 @@
 package com.practice.blog.post.dto.request;
 
-//import com.practice.blog.account.entity.Account;
-//import com.practice.blog.post.domain.Post;
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
-//import jakarta.validation.constraints.Size;
-//
-//public record PostCreateRequest(// 계정 아이디
-//                                // 제목 (빈 내용 x)
-//                                // 내용 (5자 ~ 500자)) {
-//
-//
-//}
+import com.practice.blog.account.entity.Account;
+import com.practice.blog.post.domain.Post;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record PostCreateRequest(@NotNull Long accountId,
+                                @NotBlank(message="제목을 입력해야 합니다.") String title,
+                                @Size(min=5, max=500, message="내용은 5자이상 500자 이하로 입력해야합니다.") String content) {
+    public Post toEntity(Account account) {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .writer(account)
+                .build();
+    }
+
+
+}
