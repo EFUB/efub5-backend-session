5b3889102b286aff76787a9cf3720f138374a918
cotton <72211340+kimeunsom@users.noreply.github.com>
2025-04-11 22:49:10 +0900

    5주차 세미나 post 도메인 구현

--- blog/src/main/java/com/practice/blog/post/dto/request/PostCreateRequest.java
+++ blog/src/main/java/com/practice/blog/post/dto/request/PostCreateRequest.java
@@ -1,14 +1,22 @@
 package com.practice.blog.post.dto.request;
 
-//import com.practice.blog.account.entity.Account;
-//import com.practice.blog.post.domain.Post;
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
-//import jakarta.validation.constraints.Size;
-//
-//public record PostCreateRequest(// 계정 아이디
-//                                // 제목 (빈 내용 x)
-//                                // 내용 (5자 ~ 500자)) {
-//
-//
-//}
+import com.practice.blog.account.entity.Account;
+import com.practice.blog.post.domain.Post;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record PostCreateRequest(@NotNull Long accountId,// 계정 아이디
+                                @NotBlank(message = "제목을 입력하세요.") String title, // 제목 (빈 내용 x)
+                                @Size(min = 5, max = 500, message="내용은 5자 이상 500자 이하") String content)// 내용 (5자 ~ 500자))
+
+{
+
+    public Post toEntity(Account account) {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .writer(account)
+                .build();
+    }
+}
