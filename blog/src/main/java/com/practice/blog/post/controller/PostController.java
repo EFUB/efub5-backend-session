1bde5ddcd868e01272eae41de73c61056f77f22f
jettieb <bokjihee@naver.com>
2025-04-15 21:18:00 +0900

    feat: comment crud api

--- blog/src/main/java/com/practice/blog/post/controller/PostController.java
+++ blog/src/main/java/com/practice/blog/post/controller/PostController.java
@@ -2,8 +2,8 @@ package com.practice.blog.post.controller;
 
 import com.practice.blog.post.dto.request.PostCreateRequest;
 import com.practice.blog.post.dto.request.PostUpdateRequest;
-import com.practice.blog.post.dto.response.PostListResponse;
 import com.practice.blog.post.dto.response.PostResponse;
+import com.practice.blog.post.dto.response.PostListResponse;
 import com.practice.blog.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +20,9 @@ public class PostController {
 
     // 게시물 생성
     @PostMapping
-    public ResponseEntity<Void> createPost(@Valid @RequestBody PostCreateRequest postCreateRequest){
-        Long postId = postService.createPost(postCreateRequest);
-        return ResponseEntity.created(URI.create("/posts/" + postId)).build();
-        //location 헤더에 만든 uri를 넣어줌. 프론트가 편함!
+    public ResponseEntity<Void> createPost(@Valid @RequestBody PostCreateRequest request) {
+        Long id = postService.createPost(request);
+        return ResponseEntity.created(URI.create("/posts/"+id)).build();
     }
 
     // 게시물 목록 조회
@@ -32,24 +31,20 @@ public class PostController {
         return ResponseEntity.ok(postService.getAllPosts());
     }
 
-    // 게시물 내용 조회
     @GetMapping("/{id}")
-    public ResponseEntity<PostResponse> getPost(@PathVariable("id") Long postId){
-        return ResponseEntity.ok(postService.getPost(postId));
+    public ResponseEntity<PostResponse> getPost(@PathVariable("id") Long id){
+        return ResponseEntity.ok(postService.getPost(id));
     }
 
-    // 게시물 내용 수정
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePostContent(@PathVariable("id") Long postId,
-                                                  //jwt 토큰에 들어있을 id, 비밀번호
                                                   @RequestHeader("Auth-Id") Long accountId,
                                                   @RequestHeader("Auth-Password") String password,
-                                                  @Valid @RequestBody PostUpdateRequest postUpdateRequest){
-        postService.updatePostContent(postId, postUpdateRequest, accountId, password);
+                                                  @RequestBody PostUpdateRequest request) {
+        postService.updatePostContent(postId, request, accountId, password);
         return ResponseEntity.noContent().build();
     }
 
-    // 게시물 삭제
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable("id") Long postId,
                                            @RequestHeader("Auth-Id") Long accountId,
--- blog/src/main/java/com/practice/blog/post/service/PostService.java
+++ blog/src/main/java/com/practice/blog/post/service/PostService.java
@@ -1,15 +1,15 @@
 package com.practice.blog.post.service;
 
 import com.practice.blog.account.entity.Account;
-import com.practice.blog.account.repository.AccountsRepository;
+import com.practice.blog.account.service.AccountsService;
 import com.practice.blog.global.exception.BlogException;
 import com.practice.blog.global.exception.ExceptionCode;
 import com.practice.blog.post.domain.Post;
+import com.practice.blog.post.dto.summary.PostSummary;
 import com.practice.blog.post.dto.request.PostCreateRequest;
 import com.practice.blog.post.dto.request.PostUpdateRequest;
-import com.practice.blog.post.dto.response.PostListResponse;
 import com.practice.blog.post.dto.response.PostResponse;
-import com.practice.blog.post.dto.summary.PostSummary;
+import com.practice.blog.post.dto.response.PostListResponse;
 import com.practice.blog.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,20 +22,19 @@ import java.util.List;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final AccountsRepository accountsRepository;
+    private final AccountsService accountsService;
 
     @Transactional
     public Long createPost(PostCreateRequest postCreateRequest) {
         Long accountId = postCreateRequest.accountId();
-        Account writer = findByAccountId(accountId);
-        Post newPost = postCreateRequest.toEntity(writer);
+        Account writerAccount = accountsService.findByAccountId(accountId);
+        Post newPost = postCreateRequest.toEntity(writerAccount);
         postRepository.save(newPost);
         return newPost.getId();
     }
 
     @Transactional
     public PostResponse getPost(Long postId) {
-        //게시물 조회 시 조회수 상승
         postRepository.increaseViewCount(postId);
         Post post = findByPostId(postId);
         return PostResponse.from(post);
@@ -43,44 +42,37 @@ public class PostService {
 
     @Transactional(readOnly = true)
     public PostListResponse getAllPosts() {
-        List<PostSummary> postSummaries = postRepository.findByOrderByCreatedAtDesc()
-                .stream()
-                .map(PostSummary::from).toList();   //PostSummary 함수의 from 사용
+        List<PostSummary> postSummaries = postRepository.findAllByOrderByCreatedAtDesc().stream()
+                .map(PostSummary::from).toList();
         return new PostListResponse(postSummaries, postRepository.count());
     }
 
     @Transactional
     public void updatePostContent(Long postId, PostUpdateRequest request, Long accountId, String password) {
         Post post = findByPostId(postId);
-        //accountId와 password로 작성자가 맞는지 확인.
-        Account account = findByAccountId(accountId);
+        Account account = accountsService.findByAccountId(accountId);
         authorizePostWriter(post, account, password);
-
         post.changeContent(request.content());
     }
 
     @Transactional
     public void deletePost(Long postId, Long accountId, String password) {
         Post post = findByPostId(postId);
-        //accountId와 password로 작성자가 맞는지 확인.
-        Account account = findByAccountId(accountId);
+        Account account = accountsService.findByAccountId(accountId);
         authorizePostWriter(post, account, password);
         postRepository.delete(post);
     }
 
-    private Post findByPostId(Long postId) {
+    @Transactional(readOnly = true)
+    public Post findByPostId(Long postId) {
         return postRepository.findById(postId)
-                .orElseThrow(() -> new BlogException(ExceptionCode.POST_NOT_FOUND));
-    }
-
-    private Account findByAccountId(Long accountId) {
-        return accountsRepository.findByAccountId(accountId)
-                .orElseThrow(() -> new BlogException(ExceptionCode.ACCOUNT_NOT_FOUND));
+                .orElseThrow(()-> new BlogException(ExceptionCode.POST_NOT_FOUND));
     }
 
     private void authorizePostWriter(Post post, Account account, String password) {
-        if(!post.getWriter().equals(account) || !post.getWriter().getPassword().equals(password)){
+        if(!post.getWriter().equals(account) || !post.getWriter().getPassword().equals(password)) {
             throw new BlogException(ExceptionCode.POST_ACCOUNT_MISMATCH);
         }
     }
+
 }
