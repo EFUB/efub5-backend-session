f0b9429c777fb983278317a1493b3bca9f93717d
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-04-15 19:56:07 +0900

    Feat: Comment Create API

--- blog/src/main/java/com/practice/blog/comment/service/CommentService.java
+++ blog/src/main/java/com/practice/blog/comment/service/CommentService.java
@@ -1,31 +1,42 @@
-//package com.practice.blog.comment.service;
-//
+package com.practice.blog.comment.service;
+
 //import com.practice.blog.account.dto.response.AccountCommentResponse;
-//import com.practice.blog.account.entity.Account;
-//import com.practice.blog.comment.domain.Comment;
-//import com.practice.blog.comment.dto.request.CommentRequest;
-//import com.practice.blog.comment.repository.CommentRepository;
-//import com.practice.blog.post.domain.Post;
+import com.practice.blog.account.entity.Account;
+import com.practice.blog.account.service.AccountsService;
+import com.practice.blog.comment.domain.Comment;
+import com.practice.blog.comment.dto.request.CommentRequest;
+import com.practice.blog.comment.repository.CommentRepository;
+import com.practice.blog.post.domain.Post;
 //import com.practice.blog.post.dto.response.PostCommentResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class CommentService {
-//
-//    private final AccountsService accountsService;
-//    private final PostService postService;
-//    private final CommentRepository commentRepository;
-//
-//    //댓글 생성
-//
-//    //postId로 댓글 목록 조회
-//
-//    //accountId로 댓글 목록 조회
-//
-//}
+import com.practice.blog.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class  CommentService {
+
+    private final AccountsService accountsService;
+    private final PostService postService;
+    private final CommentRepository commentRepository;
+
+    //댓글 생성
+    @Transactional
+    public Long createComment(Long postId, CommentRequest commentRequest) {
+        Long accountId = commentRequest.getAccountId();
+        Account account = accountsService.findByAccountId(accountId);
+        Post post = postService.findByPostId(postId);
+        Comment newComment = commentRequest.toEntity(account, post);
+        commentRepository.save(newComment);
+        return newComment.getId();
+    }
+
+    //postId로 댓글 목록 조회
+
+    //accountId로 댓글 목록 조회
+
+}
