c4b42a13811bfe0b8b8af41f1da1d49e68ff6454
gimye <gimyedev@gmail.com>
2025-04-17 16:38:19 +0900

    Feat : comment 기능 개발

--- blog/src/main/java/com/practice/blog/post/dto/request/PostCreateRequest.java
+++ blog/src/main/java/com/practice/blog/post/dto/request/PostCreateRequest.java
@@ -1,14 +1,19 @@
 package com.practice.blog.post.dto.request;
 
-//import com.practice.blog.account.entity.Account;
-//import com.practice.blog.post.domain.Post;
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
-//import jakarta.validation.constraints.Size;
-//
-//public record PostCreateRequest(// 계정 아이디
-//                                // 제목 (빈 내용 x)
-//                                // 내용 (5자 ~ 500자)) {
-//
-//
-//}
+import com.practice.blog.account.entity.Account;
+import com.practice.blog.post.domain.Post;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record PostCreateRequest(@NotNull Long accountId, @NotBlank String title, @Size(min=5, max=500) String content) {
+
+    public Post toEntity(Account account) {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .writer(account)
+                .build();
+    }
+
+}
