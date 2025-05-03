bbd17fe4156993065995e9f2deba219dab67c0bf
sohyu-na <solim73@naver.com>
2025-05-03 16:37:28 +0900

    feat: comment Create/Read api

--- blog/src/main/java/com/practice/blog/post/service/PostService.java
+++ blog/src/main/java/com/practice/blog/post/service/PostService.java
@@ -2,6 +2,7 @@ package com.practice.blog.post.service;
 
 import com.practice.blog.account.entity.Account;
 import com.practice.blog.account.repository.AccountsRepository;
+import com.practice.blog.account.service.AccountsService;
 import com.practice.blog.global.exception.BlogException;
 import com.practice.blog.global.exception.ExceptionCode;
 import com.practice.blog.post.domain.Post;
@@ -23,12 +24,12 @@ import java.util.List;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final AccountsRepository accountsRepository;
+    private final AccountsService accountsService;
 
     @Transactional
     public Long createPost(PostCreateRequest postCreateRequest) {
         Long accountId = postCreateRequest.accountId();
-        Account writerAccount = findByAccountId(accountId);
+        Account writerAccount = accountsService.findByAccountId(accountId);
         Post newPost = postCreateRequest.toEntity(writerAccount);
         postRepository.save(newPost);
         return newPost.getId();
@@ -53,7 +54,7 @@ public class PostService {
     @Transactional
     public void updatePostContent(Long postId, PostUpdateRequest request, Long accountId, String password) {
         Post post = findByPostId(postId);
-        Account account = findByAccountId(accountId);
+        Account account = accountsService.findByAccountId(accountId);
         authorizePostWriter(post, account, password);
         post.changeContent(request.content());
     }
@@ -61,20 +62,17 @@ public class PostService {
     @Transactional
     public void deletePost(Long postId, Long accountId, String password) {
         Post post = findByPostId(postId);
-        Account account = findByAccountId(accountId);
+        Account account = accountsService.findByAccountId(accountId);
         authorizePostWriter(post, account, password);
         postRepository.delete(post);
     }
 
-    private Post findByPostId(Long postId) {
+    @Transactional(readOnly = true)
+    public Post findByPostId(Long postId) {
         return postRepository.findById(postId)
                 .orElseThrow(() -> new BlogException(ExceptionCode.POST_NOT_FOUND));
     }
 
-    private Account findByAccountId(Long accountId) {
-        return accountsRepository.findById(accountId)
-            .orElseThrow(() -> new BlogException(ExceptionCode.ACCOUNT_NOT_FOUND));
-    }
     private void authorizePostWriter(Post post, Account account, String password) {
         if (!post.getWriter().equals(account) || !post.getWriter().getPassword().equals(password)) {
             throw new BlogException(ExceptionCode.POST_ACCOUNT_MISMATCH);
