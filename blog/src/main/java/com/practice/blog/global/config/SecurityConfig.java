1bf58e4291165bf2e146424caf16b3d267905217
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-10-14 10:07:12 +0900

    feat: cors 활성화 코드 추가

--- blog/src/main/java/com/practice/blog/global/config/SecurityConfig.java
+++ blog/src/main/java/com/practice/blog/global/config/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,12 +31,12 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+        return http
                 // 기본 인증 방식 비활성화 (UI 대신 토큰을 통한 인증을 사용하기 때문)
 
                 // CSRF 보호 비활성화 (토큰 기반 인증이므로 필요하지 않음)
 
-                // CORS 설정 비활성화
+                //cors 설정 추가
 
                 // 요청에 따른 인증 인가 설정
 
@@ -48,5 +51,24 @@ public class SecurityConfig {
 
                 // OAuth2 로그인 설정 - 인증된 사용자 정보(프로필)를 가져오는 방식 정의, 인증 성공시 동작을 정의하는 successHandler 설정
 
-    }
+
+
+    // ============== [ CORS 설정 Bean 추가 ] ==============
+
+        // 1. 허용할 출처(프론트엔드)를 명시
+
+
+        // 2. 허용할 HTTP 메서드(GET, POST 등)를 명시
+
+
+        // 3. 허용할 HTTP 헤더를 명시
+
+
+        // 4. 자격 증명(쿠키, 인증 헤더 등)을 허용할지 여부를 설정
+        // true로 설정해야 Authorization 헤더에 담긴 JWT 토큰을 주고받기 가능
+
+
+        // 모든 경로(/)에 대해 위에서 정의한 CORS 설정을 적용
+
+    // ===============================================
 }
