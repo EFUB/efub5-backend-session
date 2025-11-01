94855292934f016449ae18023755fb955e855ad0
sohyu-na <solim73@naver.com>
2025-11-01 16:59:04 +0900

    Revert "feat: JWT / 구글 OAuth2 실습 코드 추가"
    This reverts commit aa4ca16c24db011faa5317bc0427b68eea00f84d.

--- blog/src/main/java/com/practice/blog/global/handler/OAuth2AuthenticationSuccessHandler.java
+++ blog/src/main/java/com/practice/blog/global/handler/OAuth2AuthenticationSuccessHandler.java
@@ -18,36 +18,9 @@ import java.io.IOException;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-    private final AccountsRepository accountsRepository;
-    private final TokenProvider tokenProvider;
+public class OAuth2AuthenticationSuccessHandler {
 
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        // OAuth2 인증된 사용자 정보 가져오기
-        DefaultOAuth2User oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
 
-        // 사용자 속성에서 이메일 추출
-        String email = (String) oAuth2User.getAttributes().get("email");
 
-        // email을 통해 데이터베이스에서 User 엔티티 조회
-        Account account = accountsRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException("해당 이메일을 가진 Account를 찾을 수 없습니다. email="+email));
 
-        // AccessToken, RefreshToken 발급
-        String accessToken = tokenProvider.createAccessToken(account);
-        String refreshToken = tokenProvider.createRefreshToken(account);
-
-        // RefreshToken 저장
-        tokenProvider.saveRefreshToken(account.getAccountId(), refreshToken);
-
-        // JSON 형식 응답 설정
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        // AccessToken, RefreshToken 전달
-        String jsonResponse = String.format("{\"access_token\":\"%s\",\"refresh_token\":\"%s\"}", accessToken, refreshToken);
-        response.getWriter().write(jsonResponse);
-        response.getWriter().flush();
-    }
 }
